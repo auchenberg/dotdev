6416c03cd34f27cdbdc30bf7e9842c24873cdc3b
Kenneth Auchenberg <kenneth@auchenberg.dk>
2012-07-07 19:42:05 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+echo "*** Installing dnsmasq..."
+brew install dnsmasq
+
+echo "*** Configurating dnsmasq..."
+echo 'address=/.dev/127.0.0.1' > $(brew --prefix)/etc/dnsmasq.conf
+
+echo "*** Installing dnsmasq into LaunchDaemons..."
+sudo cp -v $(brew --prefix dnsmasq)/homebrew.mxcl.dnsmasq.plist /Library/LaunchDaemons
+sudo launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
+
+echo "*** Registerering .dev into /etc/resolver dnsmasq into LaunchDaemons..."
+sudo mkdir -v /etc/resolver
+sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/dev'
+
+echo 'Running self-check to see if .dev is working'
+if ping -oc 100 'wildcard.dev' > /dev/null; then
+    echo "*** Everything looks good. Enjoy the ride!"
+else
+    echo "*** Sorrrry. Something went wrong. Don't blame me."
+fi
