add04328444f3272bf8545559bc6ac90f541f681
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-02-07 23:18:29 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,19 @@ set -e
 # Fail fast if we're not on OS X >= 10.6.0.
 
 if [ "$(uname -s)" != "Darwin" ]; then
-  echo "Sorry, DevDNS requires Mac OS X to run." >&2
+  echo "Sorry, DEV requires Mac OS X to run." >&2
   exit 1
 fi
 
 # Fail if we don't have Homebrew or MacPorts installed
 
 if ! command -v brew >/dev/null 2>&1 && ! command -v port >/dev/null 2>&1; then
-	echo "Sorry, DevDNS requires Homebrew or MacPorts to run."
+	echo "Sorry, DEV requires Homebrew or MacPorts to run."
 	exit 1
 fi
 
 echo "***********************************"
-echo "Installing devDNS - wildcard DNS for *.dev"
+echo "Installing DEV
 echo "***********************************"
 
 # Install using Homebrew
@@ -60,7 +60,7 @@ sudo mkdir -v /etc/resolver
 sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/dev'
 
 echo 'Running self-check to see if .dev is working'
-if ping -oc 100 'wildcard.dev' > /dev/null; then
+if ping -oc 100 'kenneth.dev' > /dev/null; then
     echo "*** Everything looks good. Enjoy the ride!"
 else
     echo "*** Sorrrry. Something went wrong. Don't blame me."
